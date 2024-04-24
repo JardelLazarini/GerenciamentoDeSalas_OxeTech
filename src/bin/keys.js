f9677a8ec1eb9e88f762df95d7a4be54bd6b915e
Jardel Lazarini <90012716+Jardel1441@users.noreply.github.com>
2024-04-24 11:43:43 -0300

    caminhoBD

--- src/bin/keys.js
+++ src/bin/keys.js
@@ -3,7 +3,7 @@ module.exports = {
         port: process.env.port || 3000
     },
     database: {
-        connection: process.env.connection ||  'mongodb://127.0.0.1:27017'
+        connection: process.env.connection ||  'mongodb+srv://root:1234@mgmtroom.h65yeun.mongodb.net/?retryWrites=true&w=majority&appName=mgmtRoom'
     },
     auth: {
         secret: 'c1c2c3c4c5'
